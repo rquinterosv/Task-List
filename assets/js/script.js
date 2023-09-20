b31ee3cb0a23989b6a5a6686f4a4eda88deb82c1
rquinterosv <rafael.quinterosv@gmail.com>
2023-09-19 19:25:00 -0700

    Fixing local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const addTaskButton = document.getElementById('addTask');
     const taskList = document.getElementById('taskList');
 
+    // Load tasks from localStorage on page load
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    savedTasks.forEach(taskText => {
+        addTask(taskText);
+    });
+
+    function addTask(taskText) {
+        const taskItem = document.createElement('li');
+        taskItem.innerHTML = `
+            <span>${taskText}</span>
+            <button class="remove">Remove</button>
+        `;
+        taskList.appendChild(taskItem);
+
+        // Add event listener to remove task
+        taskItem.querySelector('.remove').addEventListener('click', function () {
+            taskItem.remove();
+            updateLocalStorage();
+        });
+    }
+
     addTaskButton.addEventListener('click', function () {
         const taskText = taskInput.value.trim();
 
         if (taskText !== '') {
-            const taskItem = document.createElement('li');
-            taskItem.innerHTML = `
-                <span>${taskText}</span>
-                <button class="remove">Remove</button>
-            `;
-            taskList.appendChild(taskItem);
-
+            addTask(taskText);
             taskInput.value = '';
-
-            // Add event listener to remove task
-            taskItem.querySelector('.remove').addEventListener('click', function () {
-                taskItem.remove();
-            });
+            updateLocalStorage();
         }
     });
 
@@ -27,6 +38,13 @@ document.addEventListener('DOMContentLoaded', function () {
     taskList.addEventListener('click', function (e) {
         if (e.target.tagName === 'SPAN') {
             e.target.classList.toggle('completed');
+            updateLocalStorage();
         }
     });
+
+    // Update localStorage with current task list
+    function updateLocalStorage() {
+        const tasks = Array.from(taskList.querySelectorAll('li span')).map(span => span.textContent);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 });
